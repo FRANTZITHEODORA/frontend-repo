5702ee2c9b36e5e32438b9b2ebeab164a8dda5dc
Ritsa <frantzi1991@gmail.com>
2024-09-18 15:54:54 +0300

    Initial commit

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    https: require.resolve('https-browserify'),
+    stream: require.resolve('stream-browserify'),
+    assert: require.resolve('assert/'),
+    url: require.resolve('url/')
+  };
+
+  return config;
+};
